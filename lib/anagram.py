a746b732501e16ac5c71d46d14676c00732ec779
daniel <danielmutisya203@gmail.com>
2023-06-13 23:57:14 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        matches = []
+        for word in possible_anagrams:
+            if sorted(word) == sorted(self.word):
+                matches.append(word)
+        return matches
